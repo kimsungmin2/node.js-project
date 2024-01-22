c754fe76c888feeca21b222ac831e66d7786a184
kimsungmin2 <154573616+kimsungmin2@users.noreply.github.com>
2024-01-23 06:02:10 +0900

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -1,16 +1,15 @@
 // schemas/index.js
 
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connect = () => {
   mongoose
-    .connect(
-      // 빨간색으로 표시된 부분은 대여한 ID, Password, 주소에 맞게끔 수정해주세요!
-      'mongodb+srv://sparta-user:aaaa4321@kimsungmin123.s14xubl.mongodb.net/?retryWrites=true&w=majority',
-      {
-        dbName: 'todo_memo', // todo_memo 데이터베이스명을 사용합니다.
-      },
-    )
+    .connect(process.env.MONGODB_URI, {
+      dbName: process.env.DB_NAME,
+    })
     .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
     .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
 };
